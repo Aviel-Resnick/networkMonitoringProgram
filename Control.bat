7ff447e8946a031c570843fad19ea919704b145a
Aviel-Resnick <aviel.resnick@gmail.com>
2015-12-18 14:20:16 -0500

    1st and Last Batch Comment Commit

--- Control.bat
+++ Control.bat
@@ -1,19 +1,21 @@
 @echo off
 
+REM Navigate out of the C# Directory and into the main folder
 cd..
 cd..
 cd..
 cd..
 cd..
 
+REM Set up the structure for the system
 if not exist "Data" mkdir Data
 cd Data
 if not exist "Refined" mkdir Refined
 cd Refined
 cd..
-
 break > arp_dump.txt
 
+REM Run an ARP Scan on the first 20 indexes of the network, and send the output to arp_dump.txt (To be filtered by substring_split)
 arp -a 192.168.1.1 >> arp_dump.txt
 arp -a 192.168.1.2 >> arp_dump.txt
 arp -a 192.168.1.3 >> arp_dump.txt
